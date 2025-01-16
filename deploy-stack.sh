0e1391e680d4af52347eeeda8f6c2fb0571bac13
Horja Răzvan <horjarazvan@gmail.com>
2025-01-16 13:01:20 +0200

    Updated deployment script

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script requires root privileges. Please run as root or use sudo."
+        exec sudo "$0" "$@"
+    fi
+}
+
 select_interface() {
     echo "Available network interfaces:"
     interfaces=$(ip -o link show | awk -F': ' '{print $2}')
@@ -67,21 +74,71 @@ deploy_stack() {
     echo "Docker stack deployed successfully."
 }
 
+stop_stack() {
+    local stack_name="${PWD##*/}-stack"
+    echo "Stopping and removing Docker stack..."
+    docker stack rm "$stack_name"
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to remove Docker stack."
+        exit 1
+    fi
+
+    echo "Docker stack removed successfully."
+}
+
+leave_swarm() {
+    echo "Leaving Docker Swarm..."
+    docker swarm leave --force
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to leave Docker Swarm."
+        exit 1
+    fi
+
+    echo "Left Docker Swarm successfully."
+}
+
+start() {
+    echo "Select the network interface for Docker Swarm."
+    select_interface
+
+    echo "Initializing Docker Swarm..."
+    initialize_swarm "$interface"
+
+    echo "Building Docker image..."
+    build_image
+
+    echo "Deploying Docker stack..."
+    deploy_stack
+
+    echo "Deployment completed successfully."
+}
+
+stop() {
+    stop_stack
+    leave_swarm
+    echo "Stopped successfully."
+}
+
+check_root
+
 if ! command -v docker &>/dev/null; then
     echo "Error: Docker is not installed or not in the PATH."
     exit 1
 fi
 
-echo "Select the network interface for Docker Swarm."
-select_interface
-
-echo "Initializing Docker Swarm..."
-initialize_swarm "$interface"
-
-echo "Building Docker image..."
-build_image
-
-echo "Deploying Docker stack..."
-deploy_stack
-
-echo "Deployment completed successfully."
+case $1 in
+    help)
+        echo "Usage: $0 {start|stop}"
+        exit 0
+        ;;
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
